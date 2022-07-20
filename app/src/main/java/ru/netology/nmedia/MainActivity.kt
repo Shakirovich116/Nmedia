4f8dbe8643285edc5458b47764df1660aaa85b68
Shakirovich116 <shakirovich116@gmail.com>
2022-07-20 17:22:40 +0300

    Sobutya

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,17 +1,65 @@
+
+
+
+
+
+
 package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.DrawableRes
+import kotlinx.android.synthetic.main.menu.*
+import ru.netology.nmedia.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
-//{
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//        println(resources.displayMetrics.heightPixels) // 1794
-//       println(resources.displayMetrics.widthPixels) // 1080
-//        println(resources.displayMetrics.densityDpi) // 420
-//        println(resources.displayMetrics.density) // 2.625
-//    }
-//}
+import ru.netology.nmedia.post.countFormat
+import ru.netology.nmedia.post.Post
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 0L,
+            author = resources.getString(R.string.author),
+            content = resources.getString(R.string.content),
+            published = resources.getString(R.string.published),
+            likes = resources.getString(R.string.likesCount).toInt(),
+            shares = resources.getString(R.string.sharesCount).toInt(),
+            views = resources.getString(R.string.visibilityCount).toInt()
+        )
+
+        binding.render(post)
+    }
+
+    private fun ActivityMainBinding.render(post: Post) {
+        author.text = post.author
+        content.text = post.content
+        published.text = post.published
+
+        likesCount?.setOnClickListener {
+            post.likedByMe = !post.likedByMe
+            likesCount.setImageResource(
+                getLikeIconResId(post.likedByMe)
+            )
+            likesCount.text = countFormat(if (post.likedByMe) (post.likes + 1) else (post.likes))
+        }
+
+        shareCount?.setOnClickListener {
+            shareCount.text = countFormat(post.shares++)
+        }
+
+
+    }
+
+    @DrawableRes
+    private fun getLikeIconResId(liked: Boolean) =
+        if (liked) R.drawable.like_red_background else R.drawable.likes_background
+
+
+}
---
+++ app/src/main/java/ru/netology/nmedia/post/post.kt
@@ -0,0 +1,45 @@
+package ru.netology.nmedia.post
+
+
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    var likes: Int = 0,
+    var shares: Int = 0,
+    var views: Int = 0,
+    var likedByMe: Boolean = false
+)
+val post = Post(
+    id = 1,
+    content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+    published = "21 мая в 18:36",
+    likedByMe = false)
+
+fun countFormat(count: Int): String {
+    return when (count) {
+        in 1..999 -> "${count.toString()}"
+        in 1000..1099 ->"${roundNoDecimal(count.toDouble()/1_000.0)}K"
+        in 1100..9_999 ->"${roundWithDecimal(count.toDouble()/1_000.0)}K"
+        in 10_000..999_999 ->"${roundNoDecimal(count.toDouble()/1_000.0)}K"
+        in 1_000_000..1_099_999 ->"${roundNoDecimal(count.toDouble()/1_000_000.0)}M"
+        in 1_100_000..Int.MAX_VALUE ->"${roundWithDecimal(count.toDouble()/1_000_000.0)}M"
+
+        else-> count.toString()
+    }
+}
+
+fun roundWithDecimal(number: Double): Double? {
+    val df = DecimalFormat("#.#")
+    df.roundingMode = RoundingMode.FLOOR
+    return df.format(number).toDouble()
+}
+fun roundNoDecimal(number: Double): Int? {
+    val df = DecimalFormat("#")
+    df.roundingMode = RoundingMode.FLOOR
+    return df.format(number).toInt()
+}
