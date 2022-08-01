701826f8421b453b4f715f94c3742c04d759f9eb
Shakirovich116 <shakirovich116@gmail.com>
2022-08-01 17:38:54 +0300

    99

--- app/src/main/java/ru/netology/nmedia/post/post.kt
+++ app/src/main/java/ru/netology/nmedia/post/post.kt
@@ -10,16 +10,11 @@ data class Post(
     val content: String,
     val published: String,
     var likesCount: Int = 0,
-    var shares: Int = 0,
-    var views: Int = 0,
+    var sharesCount: Int = 0,
+    var viewsCount: Int = 0,
     var likedByMe: Boolean = false
 )
-val post = Post(
-    id = 1,
-    author = "Нетология. Университет интернет-профессий будущего",
-    content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-    published = "21 мая в 18:36",
-    likedByMe = false)
+
 
 fun countFormat(likesCount: Int): String {
     return when (likesCount) {
